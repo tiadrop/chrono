d0741bbdb79593f7a674de9375134e89bab28502
Aleta <aletabix@pm.me>
2024-08-26 03:01:14 +0100

    init

---
+++ index.ts
@@ -0,0 +1,261 @@
+export type TimeUnit = "milliseconds" | "seconds" | "minutes" | "hours" | "days" | "weeks" | "microfortnights";
+
+export type TimeBreakdown<T extends TimeUnit> = Readonly<Record<T, number>>;
+
+type TimePointDescription = {
+    year: number;
+    month: number;
+    day: number;
+    hour: number;
+    minute: number;
+    second: number;
+    timezone: string;
+}
+
+const maxDay: number[] = [
+    31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+]
+
+export class TimePoint {
+    readonly unixEpoch: TimePeriod;
+    /**
+     * Create a TimePoint from a JavaScript Date
+     * @param unixPeriod 
+     */
+    constructor(date: Date)
+    /**
+     * Create a TimePoint from a milliseconds elapsed since 1970-01-01
+     * @param unixPeriod 
+     */
+    constructor(unixMs: number)
+    /**
+     * Create a TimePoint from a period as elapsed since 1970-01-01
+     * @param unixPeriod 
+     */
+    constructor(unixPeriod: TimePeriod)
+    /**
+     * Create a TimePoint from a string
+     * 
+     * This uses Date(string) to parse; be aware of its idiosyncrasies
+     * @param dateString
+     */
+    constructor(dateString: string)
+    /**
+     * Create a TimePoint from an object description
+     * 
+     * Description members should be as shown on a calendar and clock, ie `{ month: 2, day: 1 }` represents 1st February
+     * @param description
+     */
+    constructor(description: TimePointDescription)
+    constructor(input: Date | number | string | TimePeriod | TimePointDescription) {
+        if (typeof input == "string") input = new Date(input);
+        if (input instanceof Date) input = input.getTime();
+        if (typeof input == "number") input = new TimePeriod(input);
+        if (!(input instanceof TimePeriod)) {
+            const { year, month, day, hour, minute, second, timezone } = input;
+            if ([year, month, day, hour, minute].some(v => v % 1 > 0)) throw new Error("Invalid time descriptor (non-integral values are only allowed for 'second')");
+            if (month < 1 || month > 12) throw new Error("Invalid month");
+            if (day < 1 || day > maxDay[month - 1]) throw new Error("Invalid day"); //? todo special feb logic?
+            if (hour < 0 || hour >= 24) throw new Error("Invalid hour");
+            if (minute < 0 || minute >= 60) throw new Error("Invalid minute");
+            if (second < 0 || second >= 60) throw new Error("Invalid second");
+            const [
+                year0, month0, day0, hour0, minute0, second0
+            ] = [
+                year, month, day, hour, minute, second
+            ].map(n => (n+'').padStart(2, '0'));
+            const ms = new Date(
+                `${year0}-${month0}-${day0} ${hour0}:${minute0}:${second0} ${timezone}`
+            ).getTime();
+            input = new TimePeriod(ms);
+        }
+        this.unixEpoch = input;
+    }
+    get asDate(){
+        return new Date(this.unixEpoch.asMilliseconds);
+    }
+    isBefore(when: TimePoint | Date) {
+        const ms = when instanceof Date ? when.getTime() : when.unixEpoch.asMilliseconds;
+        return this.unixEpoch.asMilliseconds < ms;
+    }
+    isAfter(when: TimePoint | Date) {
+        const ms = when instanceof Date ? when.getTime() : when.unixEpoch.asMilliseconds;
+        return this.unixEpoch.asMilliseconds > ms;
+    }
+    add(...periods: (TimePeriod | TimeBreakdown<any>)[]) {
+        return new TimePoint(this.unixEpoch.add(...periods));
+    }
+    subtract(period: TimePeriod | TimeBreakdown<any>) {
+        return new TimePoint(this.unixEpoch.subtract(period as any));
+    }
+    difference(time: TimePoint) {
+        return time.unixEpoch.subtract(this.unixEpoch);
+    }
+    toJSON(){
+        return { unixEpoch: this.unixEpoch.breakdown() };
+    }
+    equals(point: TimePoint){
+        return this.unixEpoch.equals(point.unixEpoch);
+    }
+
+    static get epochStart() { return new TimePoint(0) }
+
+    static now() {
+        return new TimePoint(Date.now());
+    }
+}
+
+const divisors: Record<TimeUnit, number> = {
+    microfortnights: 1209.6,
+    weeks: 3600000 * 24 * 7,
+    days: 3600000 * 24,
+    hours: 3600000,
+    minutes: 60000,
+    seconds: 1000,
+    milliseconds: 1,
+};
+
+type BreakdownOptions<Z extends boolean> = {
+    floatLast: boolean;
+    includeZero: Z;
+}
+
+type DefaultBreakdownUnits = "days" | "hours" | "minutes" | "seconds" | "milliseconds";
+
+export class TimePeriod {
+    readonly asMilliseconds: number;
+    constructor(milliseconds: number)
+    constructor(breakdown: Partial<TimeBreakdown<TimeUnit>>)
+    constructor(t: number | Partial<TimeBreakdown<TimeUnit>>) {
+        if (typeof t == "number") {
+            this.asMilliseconds = t;
+            return;
+        }
+        this.asMilliseconds = Object.entries(t).reduce(
+            (sum, [unit, amount]) => sum + divisors[unit as TimeUnit] * amount,
+            0
+        );
+    }
+    get asSeconds(){
+        return this.asMilliseconds / divisors.seconds;
+    }
+    get asMinutes(){
+        return this.asMilliseconds / divisors.minutes;
+    }
+    get asHours(){
+        return this.asMilliseconds / divisors.hours;
+    }
+    get asDays(){
+        return this.asMilliseconds / divisors.days;
+    }
+    get asWeeks(){
+        return this.asMilliseconds / divisors.weeks;
+    }
+    add(...periods: (TimePeriod | Partial<TimeBreakdown<TimeUnit>>)[]): TimePeriod {
+        return new TimePeriod({
+            milliseconds: periods.reduce(
+                (sum, p) => sum + (
+                    p instanceof TimePeriod ? p : new TimePeriod(p)
+                ).asMilliseconds,
+                this.asMilliseconds
+            ),
+        });
+    }
+    subtract(period: TimePeriod): TimePeriod
+    subtract(breakdown: TimeBreakdown<any>): TimePeriod
+    subtract(period: TimePeriod | TimeBreakdown<any>): TimePeriod {
+        return new TimePeriod({
+            milliseconds: this.asMilliseconds - (
+                period instanceof TimePeriod ? period : new TimePeriod(period)
+            ).asMilliseconds,
+        });
+    }
+    /**
+     * Returns an object that describes this period in desired units
+     * 
+     * eg `{ hours: 5, minutes: 30, seconds: 10 }`
+     * @param units List of desired units as strings - eg `["hours", "minutes", "seconds"]`
+     * @param options Defaults when units are provided: `{ floatLast: false, includeZero: true }`
+     */
+    breakdown<T extends TimeUnit, Z extends boolean = true>(
+        units: T[],
+        options?: Partial<BreakdownOptions<Z>>
+    ): Z extends true
+        ? TimeBreakdown<T>
+        : Partial<TimeBreakdown<T>>
+    /**
+     * Returns an object that describes this period using units days, hours, minutes, seconds and milliseconds
+     * 
+     * eg `{ hours: 5, minutes: 30, seconds: 10 }`
+     * @param options Defaults when units are not provided: `{ floatLast: true, includeZero: false }`
+     */
+    breakdown<Z extends boolean>(
+        options?: Partial<BreakdownOptions<Z>>
+    ): Z extends true
+        ? TimeBreakdown<DefaultBreakdownUnits>
+        : Partial<TimeBreakdown<DefaultBreakdownUnits>>
+    breakdown(
+        units: TimeUnit[] | Partial<BreakdownOptions<any>> = {},
+        options: Partial<BreakdownOptions<any>> = {}
+    ) {
+        let fullOptions: BreakdownOptions<any>;
+        if (Array.isArray(units)) {
+            // units provided
+            fullOptions = {
+                floatLast: false,
+                includeZero: true,
+                ...options,
+            }
+        } else {
+            // units not provided; units is options
+            fullOptions = {
+                floatLast: true,
+                includeZero: false,
+                ...units,
+            };
+            units = ["days", "hours", "minutes", "seconds", "milliseconds"];
+        }
+
+        const {floatLast, includeZero} = fullOptions;
+        let remaining = this.asMilliseconds;
+        const result: Partial<Record<TimeUnit, number>> = {};
+        const floatUnit = floatLast && units.at(-1);
+
+        units.forEach(unit => {
+            const div = divisors[unit];
+            if (unit === floatUnit) {
+                const amount = remaining / div;
+                if (includeZero || amount !== 0) result[unit] = amount;
+                return;
+            }
+            const amount = Math.floor(remaining / div);
+            remaining -= amount * div;
+            if (includeZero || amount !== 0) result[unit] = amount;
+        });
+        return result;
+    }
+    multiply(by: number) {
+        return new TimePeriod({
+            milliseconds: this.asMilliseconds * by,
+        })
+    }
+    divide(by: number) {
+        return new TimePeriod({
+            milliseconds: this.asMilliseconds / by,
+        })
+    }
+    equals(period: TimePeriod) {
+        return period.asMilliseconds == this.asMilliseconds;
+    }
+    toJSON() {
+        return { milliseconds: this.asMilliseconds };
+    }
+
+    static weeks(n: number) { return new TimePeriod({weeks: n}) }
+    static days(n: number) { return new TimePeriod({days: n}) }
+    static hours(n: number) { return new TimePeriod({hours: n}) }
+    static minutes(n: number) { return new TimePeriod({minutes: n}) }
+    static seconds(n: number) { return new TimePeriod({seconds: n}) }
+    static milliseconds(n: number) { return new TimePeriod({milliseconds: n}) }
+
+}
---
+++ tests/chrono.test.ts
@@ -0,0 +1,292 @@
+import {TimeBreakdown, TimePeriod, TimePoint, TimeUnit} from "../index"
+
+const hourAndHalf = TimePeriod.hours(1.5);
+
+describe("TimePeriod", () => {
+
+    it("should provide correct weeks, days, hours, minutes, seconds and milliseconds", () => {
+        expect(hourAndHalf.asDays).toBe(1.5 / 24);
+        expect(hourAndHalf.asHours).toBe(1.5);
+        expect(hourAndHalf.asMinutes).toBe(90);
+        expect(hourAndHalf.asSeconds).toBe(90 * 60);
+        expect(hourAndHalf.asMilliseconds).toBe(90 * 60000);
+        expect(new TimePeriod({days: 14}).asWeeks).toBe(2);
+    });
+
+    describe("construction", () => {
+        it("should instantiate given a breakdown", () => {
+            expect(new TimePeriod({
+                hours: 1,
+                minutes: 30
+            }).asMinutes).toBe(90);
+            expect(new TimePeriod({
+                hours: 1.75,
+            }).asMinutes).toBe(105);
+        });
+        
+        it("should instantiate given a number of milliseconds", () => {
+            const fromMs = new TimePeriod(1337);
+            expect(fromMs.asMilliseconds).toBe(1337);
+        });
+    });
+
+
+    describe("breakdown()", () => {
+        const period = new TimePeriod({hours: 1, minutes: 30, seconds: 15});
+
+        it("should provide automatic breakdown if no unit list is provided", () => {
+            expect(period.breakdown()).toStrictEqual({
+                hours: 1,
+                minutes: 30,
+                seconds: 15,
+            });
+            expect(period.add({milliseconds: 500}).breakdown()).toStrictEqual({
+                hours: 1,
+                minutes: 30,
+                seconds: 15,
+                milliseconds: 500,
+            });
+        });
+
+
+        it("should include zeros if includeZero option is true", () => {
+            expect(period.breakdown({
+                includeZero: true,
+            })).toStrictEqual({
+                hours: 1,
+                minutes: 30,
+                seconds: 15,
+                days: 0,
+                milliseconds: 0,
+            });
+            expect(period.breakdown(["days", "minutes"])).toStrictEqual({
+                days: 0,
+                minutes: 90
+            });
+        })
+
+        it("should provide accurate breakdown given a list of units", () => {
+            expect(period.breakdown(["hours"])).toStrictEqual({hours: 1});
+            expect(period.breakdown(["hours", "minutes"])).toStrictEqual({hours: 1, minutes: 30});
+            expect(period.breakdown(["days", "hours", "minutes"])).toStrictEqual({days: 0, hours: 1, minutes: 30});
+        });
+
+        it("should provide full float value of last unit when floatLast option is true", () => {
+            expect(period.breakdown(["hours", "minutes"], {
+                floatLast: true
+            })).toStrictEqual({
+                hours: 1,
+                minutes: 30.25,
+            });
+            expect(TimePeriod.seconds(65.3).breakdown(["minutes", "seconds"], {
+                floatLast: true
+            })).toStrictEqual({
+                minutes: 1,
+                seconds: 5.3,
+            });
+        });
+
+    });
+
+    describe("add()", () => {
+        it("should add TimePeriod", () => {
+            const period = hourAndHalf.add(TimePeriod.seconds(125));
+            expect(period.breakdown(["hours", "minutes", "seconds"])).toStrictEqual({
+                hours: 1,
+                minutes: 32,
+                seconds: 5
+            });
+        });
+
+        it("should add TimeBreakdown", () => {
+            const period = hourAndHalf.add({seconds: 125});
+            expect(period.breakdown(["hours", "minutes", "seconds"])).toStrictEqual({
+                hours: 1,
+                minutes: 32,
+                seconds: 5
+            });
+        });
+    });
+
+    describe("subtract()", () => {
+        it("should subtract TimePeriod", () => {
+            const period = hourAndHalf.subtract(TimePeriod.seconds(125));
+            expect(period.breakdown(["hours", "minutes", "seconds"])).toStrictEqual({
+                hours: 1,
+                minutes: 27,
+                seconds: 55
+            });
+        });
+
+        it("should subtract TimeBreakdown", () => {
+            const period = hourAndHalf.subtract({seconds: 125});
+            expect(period.breakdown(["hours", "minutes", "seconds"])).toStrictEqual({
+                hours: 1,
+                minutes: 27,
+                seconds: 55
+            });
+        })
+    });
+
+    it("should multiply", () => {
+        expect(hourAndHalf.multiply(3).asMilliseconds).toBe(hourAndHalf.asMilliseconds * 3);
+    });
+
+    it("should divide", () => {
+        expect(hourAndHalf.divide(3).asMilliseconds).toBe(hourAndHalf.asMilliseconds / 3);
+    });
+
+    it("should check equality", () => {
+        expect(new TimePeriod({
+            hours: 1,
+            minutes: 30
+        }).equals(hourAndHalf)).toBeTruthy();
+        expect(new TimePeriod({
+            hours: 1,
+            minutes: 30,
+            milliseconds: 1,
+        }).equals(hourAndHalf)).toBeFalsy();
+    });
+
+    it("should encode to JSON as { milliseconds }", () => {
+        const json = JSON.stringify(hourAndHalf);
+        expect(json).toBe(JSON.stringify({
+            milliseconds: hourAndHalf.asMilliseconds
+        }));
+    });
+
+    it("should provide correct periods via static methods", () => {
+        expect(TimePeriod.weeks(2).asMilliseconds).toBe(24 * 7 * 3600000 * 2);
+        expect(TimePeriod.days(2).asMilliseconds).toBe(24 * 3600000 * 2);
+        expect(TimePeriod.minutes(2).asSeconds).toBe(120);
+        expect(TimePeriod.milliseconds(1500).asSeconds).toBeCloseTo(1.5);
+    });
+
+});
+
+describe("TimePoint", () => {
+    const specialTime = new TimePoint("2021-10-31 19:30 GMT");
+
+    describe("construction", () => {
+        const baseDescriptor = {
+            year: 2021,
+            month: 10,
+            day: 31,
+            hour: 19,
+            minute: 30,
+            second: 0,
+            timezone: "GMT"
+        }
+
+        it("should instantiate given a Date", () => {
+            const now = new Date();
+            const tp = new TimePoint(now);
+            expect(tp.unixEpoch.asMilliseconds ).toBe(now.getTime());
+        });
+
+        it("should instantiate given a string", () => {
+            const s = "1992-11-24 13:37 GMT";
+            const point = new TimePoint(s);
+            const date = new Date(s);
+            expect(point.unixEpoch.asMilliseconds).toBe(date.getTime());
+        });
+
+        it("should instantiate given an epochal TimePeriod", () => {
+            const point = new TimePoint(new TimePeriod({
+                seconds: 1635706800
+            }));
+            expect(point.unixEpoch.asMilliseconds).toBe(1635706800000);
+        });
+
+        it("should instantiate given a number of milliseconds", () => {
+            const ms = 133742069;
+            const point = new TimePoint(ms);
+            expect(point.unixEpoch.asMilliseconds).toBe(ms);
+        });
+
+        it("should instantiate given a descriptor object", () => {
+            const point = new TimePoint(baseDescriptor);
+            expect(point.unixEpoch.breakdown()).toStrictEqual(specialTime.unixEpoch.breakdown());
+        });
+
+        it("should disallow invalid descriptors", () => {
+            expect(() => new TimePoint({...baseDescriptor, year: 2021.4})).toThrow();
+            expect(() => new TimePoint({...baseDescriptor, month: 0})).toThrow();
+            expect(() => new TimePoint({...baseDescriptor, second: -10})).toThrow();
+            expect(() => new TimePoint({...baseDescriptor, month: 2, day: 30})).toThrow();
+            expect(() => new TimePoint({...baseDescriptor, hour: 24})).toThrow();
+            expect(() => new TimePoint({...baseDescriptor, minute: 60})).toThrow();
+        });
+        
+    });
+
+    it("should provide an accurate JS Date", () => {
+        const s = "1992-11-24 13:37 GMT";
+        const point = new TimePoint(s);
+        const date = point.asDate;
+        expect(point.unixEpoch.asMilliseconds).toBe(date.getTime());
+    });
+
+    it("should add (mixture of) TimePeriod and TimeBreakdown", () => {
+        const weekAfterSpecial = specialTime.add(
+            TimePeriod.days(3),
+            { days: 4 }
+        );
+        expect(weekAfterSpecial.unixEpoch.asMilliseconds).toBe(
+            new Date("2021-11-07 19:30 GMT").getTime()
+        );
+    });
+
+    it("should subtract TimePeriod and TimeBreakdown", () => {
+        const weekBeforeSpecialTP = specialTime.subtract(TimePeriod.days(7));
+        expect(weekBeforeSpecialTP.unixEpoch.asMilliseconds).toBe(
+            new Date("2021-10-24 19:30 GMT").getTime()
+        );
+        const weekBeforeSpecialBD = specialTime.subtract({weeks: 1});
+        expect(weekBeforeSpecialBD.unixEpoch.asMilliseconds).toBe(
+            new Date("2021-10-24 19:30 GMT").getTime()
+        );
+    });
+
+    it("should encode to JSON as { unixMilliseconds }", () => {
+        const json = JSON.stringify(specialTime);
+        const epoch = new TimePeriod(JSON.parse(json).unixEpoch);
+        expect(specialTime.unixEpoch.asMilliseconds).toBe(epoch.asMilliseconds);
+    });
+
+    it("should check if a TimePoint comes before / after a given TimePoint or Date", () => {
+        const s = "2016-10-31 GMT";
+        const point = new TimePoint(s);
+        const date = new Date(s);
+        expect(point.isBefore(specialTime)).toBeTruthy();
+        expect(point.isAfter(specialTime)).toBeFalsy();
+        expect(specialTime.isBefore(date)).toBeFalsy();
+        expect(specialTime.isAfter(date)).toBeTruthy();
+    });
+
+    it("should provide difference between two TimePoints as a TimePeriod", () => {
+        const point = specialTime.add({
+            days: 1,
+            hours: 1,
+        });
+        const diff = specialTime.difference(point);
+        expect(diff.breakdown(["days", "minutes"])).toStrictEqual({
+            days: 1,
+            minutes: 60,
+        });
+    });
+
+    it("should check equality", () => {
+        const point = new TimePoint(specialTime.unixEpoch.asMilliseconds);
+        expect(specialTime.equals(point)).toBeTruthy();
+        expect(specialTime.equals(TimePoint.now())).toBeFalsy();
+    })
+
+    it("should provide correct now()", () => {
+        const dateNow = Date.now();
+        const tpNow = TimePoint.now();
+        const diff = Math.abs(tpNow.unixEpoch.asMilliseconds - dateNow);
+        expect(diff).toBeLessThan(50);
+    });
+
+});
